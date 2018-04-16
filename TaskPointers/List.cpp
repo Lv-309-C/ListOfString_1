c8767caa3f27f1bb7d3bde1d8ce836b78c1bbac9
Orest Andriichuk <mymarichello777@gmail.com>
2018-04-17 00:16:36 +0300

    Added functions of replacing and sorting

--- TaskPointers/List.cpp
+++ TaskPointers/List.cpp
@@ -6,35 +6,37 @@ using namespace std;
 int main()
 {
 	ListElement list = nullptr;
-	AddStringToList(&list, "Andriy");
-	AddStringToList(&list, "Orest");
-	AddStringToList(&list, "SOLID");
-	AddStringToList(&list, "Orest");
-	AddStringToList(&list, "Hello world");
-	AddStringToList(&list, "Orest");
-	AddStringToList(&list, "Orest");
+	//AddStringToList(&list, "Andriy");
+	//AddStringToList(&list, "Orest");
+	//AddStringToList(&list, "SOLID");
+	//AddStringToList(&list, "Orest is looking at the window");
+	//AddStringToList(&list, "Hello world");
+	//AddStringToList(&list, "Orest");
+	//AddStringToList(&list, "Orest");
+	AddStringToList(&list, "ABCD");
+	AddStringToList(&list, "BBCC");
+	AddStringToList(&list, "CACD");
+	AddStringToList(&list, "BCAD");
+	AddStringToList(&list, "DAAA");
+	AddStringToList(&list, "CABD");
+	AddStringToList(&list, "BCDA");
+
 
 	PrintList(list);
 	cout << "Number of items in the list: " << GetNumberOfElements(list) << endl;
 	cout << "Index of str \"Andriy\" in the list: "
 		 << GetIndexOfString(list, "andriy") << endl;
 
-	RemoveStringFromList(&list, "solid");
-	PrintList(list);
-	//PrintListReverse(list);
+
+	ReplaceStringsInList(list, "Orest", "Andriy");
 	RemoveDublicates(&list);
+	PrintList(list);
+	cout << "***" << endl;
+	
+	SortList(list, AlphabetComparator);
 	PrintList(list);
 	DestroyList(&list);
 
-	//ListElement elem = InitElementOfList();
-	//String str = "Orest";
-	//elem[Data] = (String)malloc(strlen(str) + 1);
-	//strcpy(elem[Data], str);
-	//cout << elem[Data] << endl;
-
-	//String* str2 = &elem[Data];
-	//free(elem);
-	//cout << *str2 << endl;
 
 	system("pause");
 	return 0;
--- TaskPointers/List.h
+++ TaskPointers/List.h
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 typedef char** ListElement;
 typedef char* ListField;
 typedef char* String;
@@ -18,11 +19,22 @@ void AddStringToList(ListElement*, String);
 void RemoveElement(ListElement*, ListElement);
 bool RemoveStringFromList(ListElement*, String);
 void RemoveDublicates(ListElement*);
+
 ListElement GetElementWithString(ListElement, String);
 ListElement GetLastElementOfList(ListElement);
-void PrintList(ListElement);
+
+
 int GetNumberOfElements(ListElement);
 int GetIndexOfString(ListElement, String);
+
+void ReplaceStringsInList(ListElement, String, String);
+
+void SortList(ListElement, int(*f)(const void* str1, const void* str2));
+int Partition(char**, int, int, int(*f)(const void* str1, const void* str2));
+void StringQuickSort(char**, int, int, int(*f)(const void* str1, const void* str2));
+void SwapStrings(String*, String*);
+
+void PrintList(ListElement);
 
 ListElement InitElementOfList()
 {
@@ -155,10 +167,93 @@ int GetIndexOfString(ListElement head, String str)
 			return index;
 		element = (ListElement)element[Next];
 	}
-	return index;
+	return element != nullptr? index : -1;
+}
+void ReplaceStringsInList(ListElement head, String before, String after)
+{
+	if (before == nullptr || *before == 0)
+		return;
+	if (after == nullptr || *after == 0)
+		return;
+
+	while (head != nullptr) {
+		String sub_string;
+		if ((sub_string = strstr(head[Data], before)) != NULL) {
+			String buffer = (String)malloc(strlen(head[Data]) + strlen(after));
+			
+			strcpy(buffer, head[Data]);
+			buffer[sub_string - head[Data]] = 0;
+			strcat(buffer, after);
+			strcat(buffer, sub_string + strlen(before));
+
+			head[Data] = (String)realloc(head[Data], strlen(buffer)+1);
+			strcpy(head[Data], buffer);
+			free(buffer);
+		}
+		head = (ListElement)head[Next];
+	}
+}
+int GetStringArray(ListElement head, char*** array)
+{
+	if (head == nullptr)
+		return 0;
+	if (*array != nullptr)
+		*array = nullptr;
+	int k = GetNumberOfElements(head);
+	*array = (char**)malloc(k * sizeof(String));
+	for (int i = 0; i < k; i++) {
+		(*array)[i] = head[Data];
+		head = (ListElement)head[Next];
+	}
+	return k;
+}
+int SizeComparator(void const* str1, void const* str2)
+{
+	return strlen((String)str1) - strlen((String)str2);
+}
+int AlphabetComparator(void const* str1, void const* str2)
+{
+	return _strcmpi((String)str1, (String)str2);
+}
+void SortList(ListElement head, int(*Comparator)(const void* str1, const void* str2))
+{
+	char** array_of_string = nullptr;
+	int k = GetStringArray(head, &array_of_string);
+	StringQuickSort(array_of_string, 0, k - 1, Comparator);
+	for (int i = 0; i < k; i++) {
+		head[Data] = array_of_string[i];
+		head = (ListElement)head[Next];
+	}
+	return;
+}
+void StringQuickSort(char** arr, int p, int q, int(*Comparator)(const void* str1, const void* str2))
+{
+	if (p >= q) return;
+	int x = Partition(arr, p, q, Comparator);
+	StringQuickSort(arr, p, x - 1, Comparator);
+	StringQuickSort(arr, x + 1, q, Comparator);
 }
 
+int Partition(char** arr, int p, int q, int(*Comparator)(const void* str1, const void* str2))
+{
+	char* pivot_string = arr[q];
+	int i = p;
 
+	for (int j = p; j < q; j++)
+		if ((*Comparator)(arr[i], arr[j]) > 0) {
+			SwapStrings(&arr[i], &arr[j]);
+			i++;
+		}
+	SwapStrings(&arr[i], &arr[q]);
+	return i;
+}
+void SwapStrings(String* str1, String* str2)
+{
+	String temp = *str1;
+	*str1 = *str2;
+	*str2 = temp;
+	return;
+}
 
 
 void PrintList(ListElement element)
